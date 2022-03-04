7f778f18000c43866d0e7dfd47c412568cc42dc8
erhnaks <erhnaks@hotmail.com>
2022-03-04 10:01:34 +0000

    QP-3 # New Database config, added os environment to hide secret key

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:passmysql@localhost:3306/chefdb"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'eiwogjwegwegijpoeigjgjwp'
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
 db = SQLAlchemy(app)
 from application import routes
