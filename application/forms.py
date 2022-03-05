d22abc2eb486938be2756512297bc3a6d41c5eb9
erhnaks <erhnaks@hotmail.com>
2022-03-05 16:57:58 +0000

    QP-6 # Most of the HTML files are cleaned up and tested

--- application/forms.py
+++ application/forms.py
@@ -22,15 +22,31 @@ class Updateform(FlaskForm):
             raise ValidationError ('Ooopps, This fantasy Chef name does exist, Please choose another fancy Chef name')
 
 class Pizzaform(FlaskForm):
-    name = StringField('name:', validators=[DataRequired(), Length(min=2, max=30)])
-    crust = SelectField('crust', choices=[('Thin', 'Thin'), ('Deep', 'Deep'), ('Stuffed', 'Stuffed')])
-    base = SelectField('Base Sauce', choices=[('Tomato', 'Tomato'), ('Garlic', 'Garlic'), ('Tomato&Garlic', 'Tomato&Garlic')])
-    topping1 = SelectField('Meat type', choices=[('Chicken', 'Chicken'), ('Beef', 'Beef'), ('Tuna', 'Tuna'), ('Vegetarian', 'Vegetarian')])
-    topping2 = SelectField('Spicy', choices=[('Pepper hot', 'Pepper hot'), ('Pepper mild', 'Pepper mild'), ('Jalapeno', 'Jalapeno')])
-    topping3 = SelectField('Topping', choices=[('Olives', 'Olives'), ('Onion', 'Onion'), ('Muhsroom', 'Muhsroom')])
-    topping4 = SelectField('Topping', choices=[('Sweetcorn', 'Sweetcorn'), ('Pineapple', 'Pineapple'), ('Sundried Tomato', 'Sundried Tomato')])
-    topping5 = SelectField('Exotic topping', choices=[('Garlic', 'Garlic'), ('Capsicum', 'Capsicum'), ('Eggplant', 'Eggplant')])
-    chef_id = SelectField('Pick a chef', choices=[], validators=[DataRequired()])
+    
+    name = StringField('Name your custom pizza:', validators=[DataRequired(), Length(min=2, max=30)])
+    
+    crust = SelectField('Please select crust:', 
+    choices=[('Thin', 'Thin'), ('Deep', 'Deep'), ('Stuffed', 'Stuffed')])
+    
+    base = SelectField('Pizza base:', 
+    choices=[('Tomato', 'Tomato'), ('Garlic', 'Garlic'), ('Tomato&Garlic', 'Tomato&Garlic')])
+    
+    topping1 = SelectField('Meat type:', 
+    choices=[('Chicken', 'Chicken'), ('Beef', 'Beef'), ('Tuna', 'Tuna'), ('Vegetarian', 'Vegetarian')])
+    
+    topping2 = SelectField('Choose a toping:', 
+    choices=[('Pepper hot', 'Pepper hot'), ('Pepper mild', 'Pepper mild'), ('Jalapeno', 'Jalapeno')])
+    
+    topping3 = SelectField('Choose a toping:', 
+    choices=[('Olives', 'Olives'), ('Onion', 'Onion'), ('Muhsroom', 'Muhsroom')])
+    
+    topping4 = SelectField('Choose a toping:', 
+    choices=[('Sweetcorn', 'Sweetcorn'), ('Pineapple', 'Pineapple'), ('Sundried Tomato', 'Sundried Tomato')])
+    
+    topping5 = SelectField('Choose a toping:', 
+    choices=[('Garlic', 'Garlic'), ('Capsicum', 'Capsicum'), ('Eggplant', 'Eggplant')])
+    
+    chef_id = SelectField('Pick your name', choices=[], validators=[DataRequired()])
     submit = SubmitField('Submit')
     def validate_name(self, name):
         name_pizza = Pizza.query.filter_by(name=name.data).first()
