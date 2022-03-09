d450dbdb986e08d4786f732f5747d16ed9edf6f6
Ubuntu <erhna@dev-jenkins.iokftv4xhwquzlsbffvfns5hnh.zx.internal.cloudapp.net>
2022-03-09 14:43:32 +0000

    Jenkinsfile updated

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -8,18 +8,18 @@ echo "Setup stage"
 sudo apt-get update
 sudo apt-get install -y curl jq
 
-# install Docker
+# # install Docker
 
-curl https://get.docker.com | sudo bash
+# curl https://get.docker.com | sudo bash
 
-# Install Docker compose
+# # Install Docker compose
 
-# set which version to download (latest)
-version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
-# download to /usr/local/bin/docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# make the file executable
-sudo chmod +x /usr/local/bin/docker-compose
+# # set which version to download (latest)
+# version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+# # download to /usr/local/bin/docker-compose
+# sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# # make the file executable
+# sudo chmod +x /usr/local/bin/docker-compose
 
 # Docker login to push to dockerhub
 docker login --username $DOCKER_HUB_CREDS_USR --password $DOCKER_HUB_CREDS_PSW
